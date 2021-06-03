8a84c2f2f8f7297882aa5a99c84be3e6789a463b
ka <insideka01@gmail.com>
2021-06-03 18:49:19 +0900

    전역 변수와 지역 변수의 이름이 같을 때 문제점 살펴보기

---
+++ doitc/110202.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int result;
+
+int Sum(int data1, int data2)
+{
+    int result;
+    result = data1 + data2;
+}
+
+void main()
+{
+    Sum(5, 3);
+    printf("5 + 3 = %d\n", result);
+}
