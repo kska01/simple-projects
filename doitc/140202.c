f735c2600066a7ff92dda0e5b298b4fa9a09d001
ka <insideka01@gmail.com>
2021-06-21 15:49:58 +0900

    getchar 함수를 사용하여 문자 한 개씩 두 번 입력 받기

---
+++ doitc/140202.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    int input_data;
+
+    input_data = getchar();
+    printf("first input : %c\n", input_data);
+
+    input_data = getchar();
+    printf("second input : %c\n", input_data);
+}
