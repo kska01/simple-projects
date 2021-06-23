eea91debcaa0205ba79c92b283bcc2e786ebc932
ka <insideka01@gmail.com>
2021-06-23 23:55:07 +0900

    scanf 함수를 사용하여 정수와 실수 값 입력 받기

---
+++ doitc/140401.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    int int_data;
+    float float_data;
+
+    scanf("%d", &int_data);
+    scanf("%f", &float_data);
+
+    printf("input : %d, %f\n", int_data, float_data);
+}
