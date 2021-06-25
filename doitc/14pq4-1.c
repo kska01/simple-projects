2b800c9d3250f034e24d4dbe2bdd1c640d53e42a
ka <insideka01@gmail.com>
2021-06-25 11:19:26 +0900

    14장 연습문제 4번 - for문, if문 추가 버전

---
+++ doitc/14pq4-1.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[3][16];
+    int korean[3], english[3], math[3];
+    int total[3], rank[3], i;
+
+    for(i = 0; i < 3; i++)
+    {
+        printf("%d 번째 학생 이름: ", i + 1);
+        scanf("%s", name[i]);
+        
+        printf("국어 점수: ");
+        scanf("%d", &korean[i]);
+
+        printf("영어 점수: ");
+        scanf("%d", &english[i]);
+
+        printf("수학 점수: ");
+        scanf("%d", &math[i]);
+
+        total[i] = korean[i] + english[i] + math[i];
+    }
+
+    if(total[0] > total[1])
+    {
+        if(total[1] > total[2])
+        {
+            rank[0] = 0;
+            rank[1] = 1;
+            rank[2] = 2;
+        }
+        else if(total[0] > total[2])
+        {
+            rank[0] = 0;
+            rank[1] = 2;
+            rank[2] = 1;
+        }
+        else
+        {
+            rank[0] = 2;
+            rank[1] = 0;
+            rank[2] = 1;
+        }
+    }
+    else
+    {
+        if(total[1] < total[2])
+        {
+            rank[0] = 2;
+            rank[1] = 1;
+            rank[2] = 0;
+        }
+        else if(total[0] < total[2])
+        {
+            rank[0] = 1;
+            rank[1] = 2;
+            rank[2] = 0;
+        }
+        else
+        {
+            rank[0] = 1;
+            rank[1] = 0;
+            rank[2] = 2;
+        }
+    }
+
+    printf("-------------------------------------\n");
+    printf(" 이름  국어 영어 수학 총점 평균 등수\n");
+    printf("-------------------------------------\n");
+    for(i = 0; i < 3; i++)
+    {
+        printf("%s  %2d   %2d   %2d  %3d  %3d   %d등\n", name[rank[i]], korean[rank[i]], english[rank[i]], 
+        math[rank[i]], total[rank[i]], total[rank[i]] / 3, i + 1);
+    }
+    return 0;  
+}
