49cba49931abcbd4d3c8f94d00fd1669de3703bc
ka <insideka01@gmail.com>
2021-09-30 12:01:20 +0900

    ch01 연습문제16 오른쪽 위가 직각인 삼각형

---
+++ structure_algorithm_doitc/ch01q16RU.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void triangleLB(int n)
+{
+    int i, j, k;
+    int width = n;
+
+    for (i = 1; i <= n; i++) 
+    {
+        for(k = 1; k < i; ++k)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= width; j++)
+        {
+            printf("*");
+        }
+        putchar('\n');
+        --width;
+    }
+}
+
+int main(void)
+{
+    int i, j, n;
+
+    do {
+        printf("몇 단 삼각형입니까? : ");
+        scanf("%d", &n);
+    } while (n <= 0);
+
+    triangleLB(n);
+
+    return 0;
+}
